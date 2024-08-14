88f470b4f79daf6e8b0871f016fabd9d1618028f
El Wynn <el-wynn@github.com>
2024-08-14 16:26:55 +0200

    Edit .gitignore

--- zoom_recording_launcher.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /var/services/homes/SyncSave/script/zoom-recording-downloader/env/bin/activate
-
-python3 /var/services/homes/SyncSave/script/zoom-recording-downloader/zoom-recording-downloader.py
